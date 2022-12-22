8873e96cebc8068f44f2f805d1e3e53815fad22f
NickGoit <113237474+NickGoit@users.noreply.github.com>
2022-12-22 20:19:04 +0200

    Update fields_for_record.py
    add class field

--- fields_for_record.py
+++ fields_for_record.py
@@ -1,5 +1,18 @@
 class Field:
-    pass
+    def __init__(self, value):
+        self.__value = None
+        self.value = value
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, new_value):
+        self.__value = new_value
+
+    def __str__(self):
+        return f'{self.__value}'
 
 
 class Name(Field):
@@ -24,3 +37,5 @@ class Tag(Field):
 
 class Birthday(Field):
     pass
+
+
