cae17ad79bd67362b8620af02819f160284fb434
LevytskyiS <levytskyi.sv@gmail.com>
2022-12-22 15:08:22 +0100

    Update address_book.py
    1. Додав опис функцій
    2. Поправив імена функцій.
    3. У нас не буде дублікатів функціі all_birthdays. Ця функція повертає всі ДН, які є в книгі за певний проміжок часу.  А в клас Рекорд буде інша функція, яка повертає кількість днів, яка залишилась до ДН конкретної людини.

--- address_book.py
+++ address_book.py
@@ -5,30 +5,40 @@ class AddressBook(UserDict):
 
     def __init__(self):
         super().__init__()
+        self.load_address_book()
 
     def add_record(self) -> str:
+        '''Додає ім'я як ключ та об'єкт класу Рекорд як значення.'''
         pass
 
     def search_by_name(self) -> str:
+        '''Шукає телефон по імені.'''
         pass
 
     def search_in_contact_book(self) -> str:
+        '''Шукає співпадіння по цифрі в телефоні, по букві в імені, мейлу.'''
         pass
 
-    def show_all_contact(self) -> list:
+    def get_all_records(self) -> list:
+        '''Повертає список всіх контактів із їхніми даними.'''
         pass
 
     def all_birthdays(self) -> list:
+        '''Повертає список всіх днів народжень за проміжок днів заданих користувачем.'''
         pass
 
-    def delete_contact(self) -> str:
+    def delete_record(self) -> str:
+        '''Видаляє контакт повністю.'''
         pass
 
     def save_address_book(self) -> str:
+        '''Зберігає адресну книгу'''
         pass
 
     def load_address_book(self) -> str:
+        '''Завантажує адресну книгу.'''
         pass
 
     def iterator(self) -> list:
+        '''Повертає кількість сторінок, вказаних користувачем.'''
         pass
