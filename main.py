98feeaf4460bc7ed59c98901ffa50a6caa414715
LevytskyiS <levytskyi.sv@gmail.com>
2022-12-23 14:43:15 +0100

    Update main.py
    В файле мейн создал следующие функции: add_mail_func, change_mail_func, delete_mail_func и поставил заглушки pass.

--- main.py
+++ main.py
@@ -56,6 +56,18 @@ def del_phone_func(args: list) -> str:
     pass
 
 @input_error
+def add_mail_func(args: list) -> str:
+    pass
+
+@input_error
+def change_mail_func(args: list) -> str:
+    pass
+
+@input_error
+def delete_mail_func(args: list) -> str:
+    pass
+
+@input_error
 def show_all_func(*_) -> str:
     pass
 
