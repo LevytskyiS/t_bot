a11be01c8cc357ab00d3e285ec1ce6bc597b6afa
Vladyslav Chaban <dma.employee.v@gmail.com>
2022-12-24 01:25:21 +0200

    added phone_func
    and changed some text in help_func

--- address_book.py
+++ address_book.py
@@ -14,10 +14,6 @@ class AddressBook(UserDict):
         self.data[record.name.value] = record
         return f'New contact was added successfuly.'
 
-    def search_by_name(self) -> str:
-        '''Шукає телефон по імені.'''
-        pass
-
     def search_in_contact_book(self) -> str:
         '''Шукає співпадіння по цифрі в телефоні, по букві в імені, мейлу.'''
         pass
