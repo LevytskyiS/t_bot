c92e88cbf0875c00511ed4a9cde69270c426d373
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2022-12-26 22:03:09 +0200

    fix_by_2-3-5

--- t_bot/user_funcs.py
+++ t_bot/user_funcs.py
@@ -181,7 +181,7 @@ def delete_mail_func(args: list) -> str:
 @input_error
 def show_all_func(*_) -> dict:
     '''Shows all contacts and their records.'''
-    return address_book
+    return address_book.search_in_contact_book("")
 
 @input_error
 def add_birth_func(args: list) -> str:
@@ -386,8 +386,10 @@ def handler(input_string: str) -> list:
             data = input_string[len(command):].strip()
             break
 
-    if not command and \
-        input(f"If you mean '{perhaps_command}' enter 'y': ") == "y":
+    if (
+        not command and
+        input(f"If you mean '{perhaps_command}' enter 'y': ") == "y"
+        ):
 
         command = perhaps_command
         input_string = input_string.split()[len(command.split()):]
